d408f1a8f158b5089cb3a4b5a8d6ad72d3711481
Kars Talens <karstalens@gmail.com>
2024-03-11 12:28:28 +0100

    revamp-admin-page

--- snackbezorgd-backend/snackbezorgd/snackbezorgd/settings.py
+++ snackbezorgd-backend/snackbezorgd/snackbezorgd/settings.py
@@ -83,10 +83,21 @@ WSGI_APPLICATION = 'snackbezorgd.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "sbpg",
+        "USER": "sbuserpg",
+        "PASSWORD": "zCLarbdXSPrA",
+        "HOST": "84.247.10.106",
+        "PORT": "5432",
     }
 }
 
--- snackbezorgd-backend/snackbezorgd/snackbezorgd_app/serializers.py
+++ snackbezorgd-backend/snackbezorgd/snackbezorgd_app/serializers.py
@@ -9,4 +9,9 @@ class ProductSerializer(serializers.ModelSerializer):
 class OrderSerializer(serializers.ModelSerializer):
     class Meta:
         model = order 
-        fields = ('order_number', 'first_name', 'last_name', 'email', 'time', 'paid', 'finished', 'total')
+        fields = ('order_number', 'first_name', 'last_name', 'email', 'time', 'paid', 'finished', 'products', 'total')
+
+class OrderItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = orderItem 
+        fields = ('order', 'product', 'quantity', 'subtotal')
