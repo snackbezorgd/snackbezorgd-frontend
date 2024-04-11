a2749736afdf6f12f7cd7c2685f55bf0d35a9de4
Elise Halmingh <elise.halmingh@gmail.com>
2024-04-11 12:03:09 +0200

    Sidebar styling

--- snackbezorgd/src/pages/checkout.js
+++ snackbezorgd/src/pages/checkout.js
@@ -40,7 +40,7 @@ const CheckoutPage = () => {
 
   return (
     <ThemeProvider theme={theme}>
-      <Grid container spacing={1}>
+      <Grid container spacing={1} >
         <Grid item xs={12} md={8} sx={{ marginTop: '11vh' }}>
           <Box
             sx={{
@@ -48,7 +48,7 @@ const CheckoutPage = () => {
               borderRadius: 5,
               padding: 2,
               width: '100%',
-              marginLeft: '13vh',
+              marginLeft: '6%',
             }}
           >
             <Typography variant="h6" gutterBottom fontWeight="bold">
@@ -135,23 +135,115 @@ const CheckoutPage = () => {
             </form>
           </Box>
         </Grid>
-        <Grid item xs={12} md={4}>
-          <Box
-            sx={{
-              backgroundColor: '#fff',
-              height: '100vh',
-              marginTop: '9vh',
-              position: 'fixed',
-              width: '20%',
-              top: 0,
-              right: 0,
-              padding: 3,
-            }}
-          >
-            <Typography variant="h6" gutterBottom>
-              Order Summary
+            
+            {/* Sidebar */}
+            <Grid item xs={12} md={4} >
+              <Box
+                sx={{
+                  backgroundColor: '#fff',
+                  height: '100vh',
+                  marginTop: '9vh',
+                  position: 'fixed',
+                  width: '25%',
+                  top: 0,
+                  right: 0,
+                  padding: 5,
+                  display: 'flex',
+                  flexDirection: 'column', 
+                }}
+              >
+            <Typography variant="h6" gutterBottom fontWeight={800} sx={{ textAlign: 'center'}}>
+              Winkelmandje
             </Typography>
-          </Box>
+            {/* PRODUCT */}
+            <Box sx={{ flex: 1, overflowY: 'auto' }}> 
+              <Grid container spacing={1} alignItems="center">
+                <Grid item xs={2}>
+                  <Typography variant="subtitle2" fontWeight={800}>1x</Typography>
+                </Grid>
+                <Grid item xs={6}>
+                  <Typography variant="subtitle2" fontWeight={800}>Pokebowl large</Typography>
+                </Grid>
+                <Grid item xs={4} sx={{ textAlign: 'right' }}>
+                  <Typography variant="subtitle2" fontWeight={800}>€16,45</Typography>
+                </Grid>
+              </Grid>
+              <Grid container spacing={1} alignItems="center">
+                <Grid item xs={2}></Grid>
+                <Grid item xs={7}>
+                  <Typography variant="body2">Poké bowl met sushi rijst, Poké Citrus dressing, Zalm, Komkommer, Sesam-mix, Sesam-mix, Ja, stokjes, sojasaus en wasabi erbij, Toeslag verpakking</Typography>
+                </Grid>
+              </Grid>
+              <Divider sx={{ my: 2 }} />
+
+              {/* PRODUCT */}
+              <Grid container spacing={1} alignItems="center">
+                <Grid item xs={2}>
+                  <Typography variant="subtitle2" fontWeight={800}>1x</Typography>
+                </Grid>
+                <Grid item xs={6}>
+                  <Typography variant="subtitle2" fontWeight={800}>Gyoza met kip en groente</Typography>
+                </Grid>
+                <Grid item xs={4} sx={{ textAlign: 'right' }}>
+                  <Typography variant="subtitle2" fontWeight={800}>€16,45</Typography>
+                </Grid>
+              </Grid>
+              <Grid container spacing={1} alignItems="center">
+                <Grid item xs={2}></Grid>
+                <Grid item xs={7}>
+                  <Typography variant="body2"></Typography>
+                </Grid>
+              </Grid>
+              <Divider sx={{ my: 2 }} />
+            </Box>
+
+              {/* EXTRA KOSTEN */}
+              <Box sx={{ mt: 'auto', mb:'30%'}}>
+                <Box>
+                  <Grid container spacing={1} alignItems="center">
+                    <Grid item xs={2}></Grid>
+                    <Grid item xs={6}>
+                      <Typography variant="subtitle2">Subtotaal</Typography>
+                    </Grid>
+                    <Grid item xs={4} sx={{ textAlign: 'right' }}>
+                      <Typography variant="subtitle2" fontWeight={800}>€10,00</Typography>
+                    </Grid>
+                  </Grid>
+
+                  <Grid container spacing={1} alignItems="center">
+                    <Grid item xs={2}></Grid>
+                    <Grid item xs={6}>
+                      <Typography variant="subtitle2">Bezorgkosten</Typography>
+                    </Grid>
+                    <Grid item xs={4} sx={{ textAlign: 'right' }}>
+                      <Typography variant="subtitle2" fontWeight={800}>€15,00</Typography>
+                    </Grid>
+                  </Grid>
+                
+                  <Grid container spacing={1} alignItems="center">
+                    <Grid item xs={2}></Grid>
+                    <Grid item xs={6}>
+                      <Typography variant="subtitle2">Servicekosten</Typography>
+                    </Grid>
+                    <Grid item xs={4} sx={{ textAlign: 'right' }}>
+                      <Typography variant="subtitle2" fontWeight={800}>€20,00</Typography>
+                    </Grid>
+                  </Grid>
+                  <Divider sx={{ my: 1 }} />
+
+                  <Grid container spacing={1} alignItems="center">
+                    <Grid item xs={2}></Grid>
+                    <Grid item xs={6}>
+                      <Typography variant="subtitle2" fontWeight={800}>Totaal</Typography>
+                    </Grid>
+                    <Grid item xs={4} sx={{ textAlign: 'right' }}>
+                      <Typography variant="subtitle2" fontWeight={800}>€20,00</Typography>
+                    </Grid>
+                  </Grid>
+                </Box>
+              </Box>
+            </Box>
+
         </Grid>
       </Grid>
     </ThemeProvider>
